e2d8ce8287c0e3555b95e005bafb71048ded001e
JoseLlontop <josellontop100@gmail.com>
2024-08-19 20:00:57 -0300

    Se agrego la logica necesaria para mostrar el mensaje de bienvenida

--- app/src/main/java/utn/appmoviles/trabajopractico1/WelcomeActivity.kt
+++ app/src/main/java/utn/appmoviles/trabajopractico1/WelcomeActivity.kt
@@ -1,12 +1,18 @@
 package utn.appmoviles.trabajopractico1
 
 import android.os.Bundle
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class WelcomeActivity : AppCompatActivity() {
+
+    //Variables de Clase
+    private lateinit var username : String
+    private lateinit var messege : TextView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -16,5 +22,19 @@ class WelcomeActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        //Llamada de las funciones
+        initComponets()
+        messegeWelcome()
     }
+
+    private fun initComponets() {
+        username = intent.extras?.getString("username").orEmpty()
+        messege = findViewById<TextView>(R.id.messageWelcome)
+    }
+
+    private fun messegeWelcome() {
+        messege.text = "Hola Bienvenido $username"
+    }
+
 }
